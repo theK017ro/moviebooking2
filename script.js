35ec913b457c209c34bc861dc2923f245da347f5
Keshav sharma <kecha91933@gmail.com>
2023-03-02 03:01:20 +0530

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".top").mouseover(function () {
+  $("this").fadeIn(100).fadeOut(100).fadeIn(100);
+});
+var blink_speed = 1500;
+var t = setInterval(function () {
+  var ele = document.getElementById("k");
+  ele.style.visibility = ele.style.visibility == "hidden" ? "" : "hidden";
+}, blink_speed);
+if (a === b) {
+}
+
